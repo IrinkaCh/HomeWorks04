d49a1d242c41906b9d758ec4b053d9ba6a5b7442
IrinkaCh <123irina00@mail.ru>
2022-08-21 16:39:26 +0500

    Task 001 Find Stepen Number

---
+++ Task001FindStepenNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindStepenNumber(A));
+
+double FindStepenNumber(int A)
+{
+    double result = Math.Pow(A, B);
+    return result;
+}
