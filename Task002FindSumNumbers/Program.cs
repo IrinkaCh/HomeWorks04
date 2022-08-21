e564c92292a3dda1bf68ee4e595861be52d9f504
IrinkaCh <123irina00@mail.ru>
2022-08-21 16:46:09 +0500

    Task 002 Find Sum Numbers

---
+++ Task002FindSumNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());;
+string str = number.ToString();
+int[] array = new int[str.Length];
+
+int[] AdditionArray(int[] arr)
+{
+for( int i=0; i< str.Length; i++)
+{
+     array[i] = int.Parse(str[i].ToString());
+} return array;
+}
+
+int FindSumNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int[] addition = AdditionArray(array);
+int sum = FindSumNumbers(addition);
+System.Console.WriteLine(sum);
